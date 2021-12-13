2db53c97cd72ef3f0cda4f5a32a95de06c9c88b9
sundae9 <86897782+sundae9@users.noreply.github.com>
2021-12-13 23:12:55 +0900

    Delete collect_Filesystem.py

--- collect_Filesystem.py
+++
@@ -1,8 +0,0 @@
-import collect_MFT
-import collect_LogFile
-import collect_UsnJrnl
-
-def collect_Filesystems(module_dst_path, dbname):
-    collect_MFT.collect_MFTs(module_dst_path, dbname)
-    collect_LogFile.collect_LogFiles(module_dst_path, dbname)
-    collect_UsnJrnl.collect_UsnJrnls(module_dst_path, dbname)
